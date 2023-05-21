1149688eb59c4317afaaa412a21b78b2d949c358
Gabriela Leichtweis <gabrielaleichtweis@gmail.com>
2023-05-21 11:53:09 -0300

    atividade 1

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,30 @@
 import axios from 'axios';
 
 function atividade1() {
-  /* 
-
-*/
-  /* 
-getRepositories(repo: string) {
-    axios.get(`https://api.github.com/repos/${repo}`)
-    .then(response => {
-    console.log(response.data);
-    })
-    .catch(err => {
-    console.log('Repositório não existe');
-    })
+  async function getUserFromGithub(user: string) {
+    try {
+      let usuarios = await axios.get(`https://api.github.com/users/${user}`);
+      console.log(usuarios.data);
+    } catch {
+      console.log('Usuário não existe');
     }
-    
-    Github.getRepositories('marcelo-growdev/scrapbook-es6');
-    Github.getRepositories('marcelo-growdev/qdbqqbqwn');
-    */
-
-  function getUserFromGithub(user: string) {
-    axios
-      .get(`https://api.github.com/users/${user}`)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((err) => {
-        console.log('Usuário não existe');
-      });
   }
   getUserFromGithub('djunior97');
   getUserFromGithub('djunioriqdivqv97');
+
+  async function getRepositories(repo: string) {
+    try {
+      let respositorios = await axios.get(
+        `https://api.github.com/repos/${repo}`
+      );
+      console.log(respositorios.data);
+    } catch {
+      console.log('Repositório não existe');
+    }
+  }
+
+  getRepositories('marcelo-growdev/scrapbook-es6');
+  getRepositories('marcelo-growdev/qdbqqbqwn');
 }
 atividade1();
 
